d1bd5bc6b24edb900c700e774280076edf5a3c85
Yoseph <133876586+yoseabe@users.noreply.github.com>
2024-11-26 10:53:33 +0100

    Add files via upload

---
+++ Dursthelfer/src/Dursthelfer.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Dursthelfer;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class Dursthelfer extends Plugin implements ThemeInterface
+{
+}
